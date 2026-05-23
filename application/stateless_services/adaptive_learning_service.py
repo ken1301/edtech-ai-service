3a494a734b555759cf83c53882e125b32d270a68
DingDongDingDung <nguyenkhanhtrinh@Macbook-Pro-cua-Nguyen-Trinh.local>
2026-05-23 22:25:02 +0700

    feat (app): add new action to adaptive learning activity

--- application/stateless_services/adaptive_learning_service.py
+++ application/stateless_services/adaptive_learning_service.py
@@ -15,3 +15,18 @@ class AdaptiveLearningService:
     ) -> ExerciseForPurpose:
         """Phân tích và chọn bài tập phù hợp dựa trên hồ sơ học sinh và bài tập đã cho."""
         pass
+
+    async def update_student_profile(
+        self,
+        old_student_profile: StudentProfile,
+        new_student_profile: StudentProfile,
+    ) -> StudentProfile:
+        """Cập nhật hồ sơ học sinh dựa trên thông tin mới thu thập được sau khi học sinh hoàn thành bài tập."""
+        pass
+
+    async def summarize_student_profile(
+        self,
+        student_profile: StudentProfile
+    ) -> str:
+        """Tóm tắt hồ sơ học sinh thành một đoạn văn ngắn gọn để sử dụng trong prompt cho LLM."""
+        pass
